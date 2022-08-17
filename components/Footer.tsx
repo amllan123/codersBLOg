6d0b5e732684bddc12c60097b5f9c1e26c0e5f27
amllan123 <amllanbhukta123@gmail.com>
2022-08-17 15:47:36 +0530

    fourth commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,26 +16,22 @@ function Footer() {
   return (<>
   <footer className=' container  bottom-0  mb-0 '>
  <div className='container mx-auto flex items-center   justify-between  flex-col sm:flex-row py-8  '>
-  <a href="/" className='flex items-center justify-center md:justify-start text-gray-900'>
+  <Link href="/" className='flex items-center justify-center md:justify-start text-gray-900'>
   
    <Image src="/cod.png" height={30} width={30}/>
    <span className=' font-normal ml-4 text-primary'>
     Coders Blog
      </span>
      <div className='text-gray-600 m-2'><span className=' text-sm  font-normal text-gray-600'>| © 2022 @CodeAmllan</span> </div>
-  </a>
+  </Link>
 
   
-  <div> 
-    <ul className='flex'>
-   <li><a href={newLocal} className=' mr-3  ml-2'><FontAwesomeIcon icon={faInstagram }   style={{ fontSize: 20, color: "gray" }}/></a></li>
-   <li> <a href="#" className='mr-3 '><FontAwesomeIcon  icon={faTwitter}  style={{ fontSize: 20, color: "gray",  }} /></a></li>
+  <div className=' m-2'>
     
-   <li><a href="https://www.linkedin.com/in/amllan-bhukta-88611220b/" className='mr-3'><FontAwesomeIcon icon={faLinkedinIn}  style={{ fontSize: 20, color: "gray" }}/></a></li>
-
-    </ul>
     
-   
+    <Link href={newLocal} className=' mr-3  ml-2'><FontAwesomeIcon icon={faInstagram }   style={{ fontSize: 20, color: "gray" }}/></Link>
+    <Link href="#" className='mr-3 '><FontAwesomeIcon  icon={faTwitter}  style={{ fontSize: 20, color: "gray",  }} /></Link>
+    <Link href="https://www.linkedin.com/in/amllan-bhukta-88611220b/" className='mr-3'><FontAwesomeIcon icon={faLinkedinIn}  style={{ fontSize: 20, color: "gray" }}/></Link>
 
   </div>
   
